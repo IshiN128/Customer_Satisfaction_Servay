946e44e15da2082571259ff4bc6b890df542da5f
IshiN128 <ishingit@protonmail.com>
2022-02-19 12:17:07 +0530

    Updated

---
+++ app/src/main/java/com/example/cs/feedback2.java
@@ -0,0 +1,78 @@
+package com.example.cs;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class feedback2 extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    ImageButton  dn;
+    ImageButton ignr;
+    ImageButton AprtNo;
+    private EditText aprtNo;
+
+    private  String apartNo;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback2);
+
+        dn = findViewById(R.id.btn_dn);
+        ignr = findViewById(R.id.btn_ignore);
+        AprtNo = findViewById(R.id.btn_AptNo);
+        aprtNo = findViewById(R.id.txtbox_aptno);
+
+        dn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(feedback2.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        AprtNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Intent intent =new Intent(feedback.this,ApartmentNo.class);
+//                startActivity(intent);
+                apartNo = aprtNo.getText().toString();
+                addDataToFirestore(apartNo);//adding data to database
+
+            }
+        });
+
+
+    };
+    private void addDataToFirestore(String apartNo) {
+
+        CollectionReference dbaptno = db.collection("ApartmentNos");
+        storeData ApartmentNos = new storeData(apartNo);
+
+        dbaptno.add(ApartmentNos).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(feedback2.this, "Your Apartment No has been added..", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/cs/storeData.java
@@ -0,0 +1,21 @@
+package com.example.cs;
+
+public class storeData {
+
+    private String aptNo;
+
+    public storeData(){
+
+    }
+
+    public storeData(String aptNO){
+        this.aptNo=aptNO;
+    }
+    public String getAptNo() {
+        return aptNo;
+    }
+
+    public void setAptNo(String aptNo) {
+        this.aptNo = aptNo;
+    }
+}

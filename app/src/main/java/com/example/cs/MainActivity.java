5b234aed13fb4e9785f7dc5ecf7af2b227daf664
IshiN128 <ishingit@protonmail.com>
2022-02-19 12:16:22 +0530

    Updated

--- app/src/main/java/com/example/cs/MainActivity.java
+++ app/src/main/java/com/example/cs/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.cs;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity
 //
 //                db.collection("vote").add(test);
 
+
             }
         });
 
@@ -63,15 +67,8 @@ public class MainActivity extends AppCompatActivity
             {
                 // TODO Auto-generated method stub
 
-                Intent intent = new Intent(MainActivity.this, feedback.class);
+                Intent intent = new Intent(MainActivity.this, feedback2.class);
                 startActivity(intent);
-
-
-
-                test.put("Response", "Good");
-
-                db.collection("vote").add(test);
-
 
             }
         });
